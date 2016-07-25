76bd7d00927f324770f4c0075c9a7c3fe8c60322
beshjm <bjama045@uottawa.ca>
2016-07-24 20:11:55 -0400

    added view/chceckout

---
+++ view/checkout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Checkout</title>
+</head>
+<body>
+    <h1>Checkout</h1>
+    <div>Your total: <?= "$ " . $total ?></div>
+    <div>You order has been checked out</div>
+    <div>THANKS!</div>
+    <? session_destroy(); ?>
+    <a href="index.php">HOME</a>
+</body>
+</html>
